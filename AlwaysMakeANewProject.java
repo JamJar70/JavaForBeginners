bd836d1bc38d08cc077b62776902c016a9633afc
JamJar70 <jmosley@utcmediacityuk.org.uk>
2016-09-13 10:46:01 +0100

    Add files via upload

---
+++ AlwaysMakeANewProject.java
@@ -0,0 +1,50 @@
+package alwaysmakeanewproject;
+
+public class AlwaysMakeANewProject {
+
+    public static void main(String[] args) {
+        String colour=" purple";
+        String name=" Jayden";
+        String town=" Chandler";
+        
+        System.out.println("My favourite colour is: "+ colour);
+        System.out.println("My name is:" +name);
+        System.out.println("I grew up in:" +town);
+        
+        int number=5;
+        int age=16;
+        int shoeSize=8;
+        
+        System.out.println("Your chosen number was: "+ number);
+        System.out.println("Your age is: "+age);
+        System.out.println("Your shoe size is: "+shoeSize);
+        
+        double test=1.5;
+        double pie=3.14;
+        
+        System.out.println("test");
+        System.out.println("pie");
+        
+        // Area of circle = pi*r^2
+        // Speed = distance/time
+        // Muliplication= * [8+Shift]
+        // Division= /
+        
+        System.out.println(age+shoeSize);
+        System.out.println(age-shoeSize);
+        System.out.println(age*shoeSize);
+        System.out.println(age/shoeSize);
+       
+        int x=10;
+        int y=11;
+        
+        if(x < y) {
+            //if the condition is true
+            System.out.println("x is less than y");
+        }else{
+            //if the condition is false
+            System.out.println("x is more than y");
+        }
+        
+        
+    }
